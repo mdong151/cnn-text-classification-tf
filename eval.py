56fcaa4e978f65b484a2875762c7bf402778aee9
ManhDong Nguyen <manhdong.nguyen@binance.com>
2021-04-11 20:59:16 +0700

    modify eval

--- eval.py
+++ eval.py
@@ -28,8 +28,9 @@ with open("config.yml", 'r') as ymlfile:
 
 # Eval Parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_string("checkpoint_dir", "", "Checkpoint directory from training run")
-tf.flags.DEFINE_boolean("eval_train", False, "Evaluate on all training data")
+tf.flags.DEFINE_string("checkpoint_dir", "./runs/1618133504/checkpoints", "Checkpoint directory from training run")
+tf.flags.DEFINE_boolean("eval_train", True, "Evaluate on all training data")
+tf.flags.DEFINE_boolean("enable_word_embeddings", False, "Enable/disable the word embedding (default: True)")
 
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
@@ -37,7 +38,7 @@ tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on 
 
 
 FLAGS = tf.flags.FLAGS
-FLAGS._parse_flags()
+FLAGS.flag_values_dict()
 print("\nParameters:")
 for attr, value in sorted(FLAGS.__flags.items()):
     print("{}={}".format(attr.upper(), value))
@@ -88,8 +89,17 @@ with graph.as_default():
     sess = tf.Session(config=session_conf)
     with sess.as_default():
         # Load the saved meta graph and restore variables
-        saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
-        saver.restore(sess, checkpoint_file)
+        
+        # saver = tf.compat.v1.train.import_meta_graph("{}.meta".format("./runs/1618133504/checkpoints/model-1000"))
+        # saver.restore(sess, "./runs/1618133504/checkpoints/model-1000")
+
+        if FLAGS.enable_word_embeddings:
+            saver = tf.compat.v1.train.import_meta_graph("{}.meta".format("./runs/1618117237/checkpoints/model-1700"))
+            saver.restore(sess, "./runs/1618117237/checkpoints/model-1700")
+        else :
+            saver = tf.compat.v1.train.import_meta_graph("{}.meta".format("./runs/1618133504/checkpoints/model-1000"))
+            saver.restore(sess, "./runs/1618133504/checkpoints/model-1000")
+
 
         # Get the placeholders from the graph by name
         input_x = graph.get_operation_by_name("input_x").outputs[0]
